eebbff2e51103bd3b85cfa1194d5e4e627d33429
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-27 15:00:08 -0500

    Modified the profile page

--- profile.php
+++ profile.php
@@ -22,5 +22,20 @@ $page_title=$profileData->firstName." ".$profileData->lastName ." | LinkedIn";
 ?>
 <?php require_once "backend/shared/loginHeader.php"; ?>
 <body>
-    <header></header>
+    <header id="global-nav">
+        <div class="global-nav__content">
+            <div class="top-left-part">
+                <h1 class="global-nav__branding">
+                        <a href="<?php echo url_for('home'); ?>" class="site-logo">
+                            <img src="<?php echo url_for('frontend/assets/images/brand.svg'); ?>" alt="Site Logo" title="LinkedIn">
+                        </a>
+                </h1>
+                <div class="global-nav__search">
+                        <input type="text" name="main-search" class="search-global" id="search-global" placeholder="Search" role="combobox" aria-label="Search">
+                </div>
+            </div>
+            <nav class="global-nav__nav"></nav>
+        </div>
+    
+    </header>
 </body>

70a2f85aef4c1a8a1f5b30385b0a6f67250ff81e
glikpochristopher <chris.glikpo@netwareitsolutions.com>
2020-12-28 11:07:40 -0500

    Added croppper js

--- frontend/assets/js/profile.js
+++ frontend/assets/js/profile.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
+    var cropper;
     let coverBtn=document.getElementById("cover-photo");
+    let previewContainer=document.querySelector(".artdeco-modal-step");
     let modal=document.getElementById("modal");
     $(document).on("click","#cover-photo",function(){
+        previewContainer.style.display="none";
         modal.style.display="block";
     });
 
@@ -14,4 +17,31 @@ $(document).ready(function(){
             modal.style.display="none";
         }
     }
+
+    $("#uploadCoverPhoto").change(function(e){
+        if(this.files && this.files[0]){
+            let stepModal=document.querySelector(".artdeco-modal");
+        let previewContainer=document.querySelector(".artdeco-modal-step");
+        stepModal.style.display="none";
+        previewContainer.style.display="block";
+            let reader=new FileReader();
+            reader.onload=function(e){
+
+                var image=document.getElementById("imagePreview");
+                image.src=e.target.result;
+                // $("#imagePreview").attr("src",e.target.result);
+
+                if(cropper !==  undefined){
+                    cropper.destroy();
+                }
+
+                cropper=new Cropper(image,{
+                    aspectRatio:1/1,
+                    background:false
+                });
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+   
 });

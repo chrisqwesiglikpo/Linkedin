a3a6f07337b095f0ba13ca1da83c791c9edbac64
chrisqwesiglikpo <danielqwesiotu@gmail.com>
2020-12-29 11:40:46 -0800

    Completed the UI for profileHeader

--- profile.php
+++ profile.php
@@ -12,6 +12,9 @@ if(Login::isLoggedIn()){
 if(isset($_GET['username']) == true && empty($_GET['username']) === false){
     $username =h($_GET['username']);
     $profileId = $loadFromUser->userIdByUsername($username);
+    if(empty($profileId)){
+        redirect_to(url_for('profile'));
+    }
 }
 else{
 $profileId =$user_id;
@@ -45,31 +48,41 @@ $page_title=$profileData->firstName." ".$profileData->lastName ." | LinkedIn";
                 <ul class="global-nav__primary-items">
                     <li class="global-nav-primary-item">
                         <a href="<?php echo url_for('feed'); ?>" class="global-nav__primary-link ember-view">
-                                 <svg viewBox="0 0 24 24" width="24px" height="24px" x="0" y="0" preserveAspectRatio="xMinYMin meet" class="nav-icon" focusable="false" xmlns="http://www.w3.org/2000/svg"><path d="M22,9.45,12.85,3.26A1.52,1.52,0,0,0,12,3a1.49,1.49,0,0,0-.84.26L2,9.45,3.06,11,4,10.37V20a1,1,0,0,0,1,1h5V16h4v5h5a1,1,0,0,0,1-1V10.37l.94.63Z" class="active-item" style="fill-opacity: 1"/><path d="M22,9.45L12.85,3.26a1.5,1.5,0,0,0-1.69,0L2,9.45,3.06,11,4,10.37V20a1,1,0,0,0,1,1h6V16h2v5h6a1,1,0,0,0,1-1V10.37L20.94,11ZM18,19H15V15a1,1,0,0,0-1-1H10a1,1,0,0,0-1,1v4H6V8.89l6-4,6,4V19Z" class="inactive-item" style="fill: currentColor"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="nav-icon" width="24" height="24" focusable="false">
+                          <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7z"/>
+                        </svg>
                                  <span class="global-nav__primary-link-text">Home</span>
                         </a>
                     </li>
                     <li class="global-nav-primary-item">
                         <a href="<?php echo url_for('mynetwork'); ?>" class="global-nav__primary-link ember-view">
-                                <svg viewBox="0 0 24 24" width="24px" height="24px" x="0" y="0" preserveAspectRatio="xMinYMin meet" class="nav-icon" focusable="false" xmlns="http://www.w3.org/2000/svg"><path d="M16,17.85V20a1,1,0,0,1-1,1H1a1,1,0,0,1-1-1V17.85a4,4,0,0,1,2.55-3.73l2.95-1.2V11.71l-0.73-1.3A6,6,0,0,1,4,7.47V6a4,4,0,0,1,4.39-4A4.12,4.12,0,0,1,12,6.21V7.47a6,6,0,0,1-.77,2.94l-0.73,1.3v1.21l2.95,1.2A4,4,0,0,1,16,17.85Zm4.75-3.65L19,13.53v-1a6,6,0,0,0,1-3.31V9a3,3,0,0,0-6,0V9.18a6,6,0,0,0,.61,2.58A3.61,3.61,0,0,0,16,13a3.62,3.62,0,0,1,2,3.24V21h4a1,1,0,0,0,1-1V17.47A3.5,3.5,0,0,0,20.75,14.2Z" class="active-item" style="fill-opacity: 1"/><path d="M20.74,14.2L19,13.54V12.86l0.25-.41A5,5,0,0,0,20,9.82V9a3,3,0,0,0-6,0V9.82a5,5,0,0,0,.75,2.63L15,12.86v0.68l-1,.37a4,4,0,0,0-.58-0.28l-2.45-1V10.83A8,8,0,0,0,12,7V6A4,4,0,0,0,4,6V7a8,8,0,0,0,1,3.86v1.84l-2.45,1A4,4,0,0,0,0,17.35V20a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V17.47A3.5,3.5,0,0,0,20.74,14.2ZM16,8.75a1,1,0,0,1,2,0v1.44a3,3,0,0,1-.38,1.46l-0.33.6a0.25,0.25,0,0,1-.22.13H16.93a0.25,0.25,0,0,1-.22-0.13l-0.33-.6A3,3,0,0,1,16,10.19V8.75ZM6,5.85a2,2,0,0,1,4,0V7.28a6,6,0,0,1-.71,2.83L9,10.72a1,1,0,0,1-.88.53H7.92A1,1,0,0,1,7,10.72l-0.33-.61A6,6,0,0,1,6,7.28V5.85ZM14,19H2V17.25a2,2,0,0,1,1.26-1.86L7,13.92v-1a3,3,0,0,0,1,.18H8a3,3,0,0,0,1-.18v1l3.72,1.42A2,2,0,0,1,14,17.21V19Zm7,0H16V17.35a4,4,0,0,0-.55-2l1.05-.4V14.07a2,2,0,0,0,.4.05h0.2a2,2,0,0,0,.4-0.05v0.88l2.53,1a1.5,1.5,0,0,1,1,1.4V19Z" class="inactive-item" style="fill: currentColor"/></svg>
-                                 <span class="global-nav__primary-link-text">My Network</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="nav-icon" width="24" height="24" focusable="false">
+                           <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"/>
+                        </svg>
+                        <span class="global-nav__primary-link-text">My Network</span>
                         </a>
                     </li>
                     <li class="global-nav-primary-item">
                         <a href="<?php echo url_for('jobs'); ?>" class="global-nav__primary-link ember-view">
-                                 <svg viewBox="0 0 24 24" width="24px" height="24px" x="0" y="0" preserveAspectRatio="xMinYMin meet" class="nav-icon" focusable="false" xmlns="http://www.w3.org/2000/svg"><path d="M2,13H22v6a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V13ZM22,8v4H2V8A1,1,0,0,1,3,7H7V6a3,3,0,0,1,3-3h4a3,3,0,0,1,3,3V7h4A1,1,0,0,1,22,8ZM15,6a1,1,0,0,0-1-1H10A1,1,0,0,0,9,6V7h6V6Z" class="active-item" style="fill-opacity: 1"/><path d="M21,7H17V6a3,3,0,0,0-3-3H10A3,3,0,0,0,7,6V7H3A1,1,0,0,0,2,8V19a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V8A1,1,0,0,0,21,7ZM9,6a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1V7H9V6ZM20,18H4V13H20v5Zm0-6H4V9H20v3Z" class="inactive-item" style="fill: currentColor"/></svg>
+                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="nav-icon" width="24" height="24" focusable="false">
+                                  <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"/>
+                                </svg>
                                  <span class="global-nav__primary-link-text">Jobs</span>
                         </a>
                     </li>
                     <li class="global-nav-primary-item">
                         <a href="<?php echo url_for('messaging'); ?>" class="global-nav__primary-link ember-view">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 12.64 13.64"><defs><style>.cls-1{fill:#1a2947;}</style></defs><title>message</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M6.32,0A6.32,6.32,0,0,0,1.94,10.87c.34.33-.32,2.51.09,2.75s1.79-1.48,2.21-1.33a6.22,6.22,0,0,0,2.08.35A6.32,6.32,0,0,0,6.32,0Zm.21,8.08L5.72,6.74l-2.43,1,2.4-3,.84,1.53,2.82-.71Z"/></g></g></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="nav-icon" width="24" height="24" focusable="false">
+                           <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"/>
+                        </svg>
                             <span class="global-nav__primary-link-text">Messaging</span>
                         </a>
                     </li>
                     <li class="global-nav-primary-item">
                         <a href="<?php echo url_for('notifications'); ?>" class="global-nav__primary-link ember-view">
-                            <svg viewBox="0 0 24 24" width="24px" height="24px" x="0" y="0" preserveAspectRatio="xMinYMin meet" class="nav-icon" focusable="false" xmlns="http://www.w3.org/2000/svg"><path d="M18.94,14H5.06L5.79,8.44A6.26,6.26,0,0,1,12,3h0a6.26,6.26,0,0,1,6.21,5.44Zm2,5-1.71-4H4.78L3.06,19a0.71,0.71,0,0,0-.06.28,0.75,0.75,0,0,0,.75.76H10a2,2,0,1,0,4,0h6.27A0.74,0.74,0,0,0,20.94,19Z" class="active-item" style="fill-opacity: 1"/><path d="M20.94,19L19,14.49s-0.41-3.06-.8-6.06A6.26,6.26,0,0,0,12,3h0A6.26,6.26,0,0,0,5.79,8.44L5,14.49,3.06,19a0.71,0.71,0,0,0-.06.28,0.75,0.75,0,0,0,.75.76H10a2,2,0,1,0,4,0h6.27A0.74,0.74,0,0,0,20.94,19ZM12,4.75h0a4.39,4.39,0,0,1,4.35,3.81c0.28,2.1.56,4.35,0.7,5.44H7L7.65,8.56A4.39,4.39,0,0,1,12,4.75ZM5.52,18l1.3-3H17.18l1.3,3h-13Z" class="inactive-item" style="fill: currentColor"/></svg>
+                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="nav-icon" width="24" height="24" focusable="false">
+                               <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"/>
+                            </svg>
                             <span class="global-nav__primary-link-text">Notifications</span>
                         </a>
                     </li>
@@ -91,22 +104,70 @@ $page_title=$profileData->firstName." ".$profileData->lastName ." | LinkedIn";
     <main class="core-rail">
         <div class="main-area">
                 <div class="profile-left-wrap">
-                    <div class="profile-cover-wrap" style="background-image:url(<?php echo url_for($profileData->profileCover); ?>)">
+                    <div class="profile-cover-wrap" style="background-image:url(<?php echo url_for($profileData->profileCover); ?>)" alt="Background Image">
                         <div class="upload-cov-opt-wrap">
-                        <?php if($profileId ==$user_id){ ?>
-                                <div class="add-cover-photo" id="cover-photo">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="#0a66c2;" width="16" height="16" focusable="false">
-                                    <path d="M14 4h-1.75l-1-2h-6.5l-1 2H2a1 1 0 00-1 1v7a1 1 0 001 1h12a1 1 0 001-1V5a1 1 0 00-1-1zm-1 7H3V8h2a3 3 0 006 0h2v3zm-2.87-3A2.13 2.13 0 118 5.88 2.13 2.13 0 0110.13 8zM13 7h-2.18a3 3 0 00-5.64 0H3V6h2l1-2h4l1 2h2v1z" fill="#0a66c2;"></path>
-                                </svg>
+                                <?php if($profileId ==$user_id){ ?>
+                                    <div class="add-cover-photo" id="cover-photo">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="#0a66c2;" width="16" height="16" focusable="false">
+                                        <path d="M14 4h-1.75l-1-2h-6.5l-1 2H2a1 1 0 00-1 1v7a1 1 0 001 1h12a1 1 0 001-1V5a1 1 0 00-1-1zm-1 7H3V8h2a3 3 0 006 0h2v3zm-2.87-3A2.13 2.13 0 118 5.88 2.13 2.13 0 0110.13 8zM13 7h-2.18a3 3 0 00-5.64 0H3V6h2l1-2h4l1 2h2v1z" fill="#0a66c2;"></path>
+                                    </svg>
+                                    </div>
+                                <?php }else{ ?>
+                                <div class="dont-add-cover-photo">
+                                
                                 </div>
-                            <?php }else{ ?>
-                            <div class="dont-add-cover-photo">
-                            
-                            </div>
-                            <?php } ?>
-                            
+                                <?php } ?>
+                                
                             
                         </div>
+                        <div class="cover-photo-rest-wrap">
+                               <?php if($profileId ==$user_id){ ?>
+                                <div class="pv-top-card--photo text-align-left">
+                                    <div class="pv-top-card__photo-wrapper ml0">
+                                      <div class="profile-pic">
+                                        <button aria-label="Profile photo Btn" class="pv-top-card__edit-photo-button" type="button">
+                                            <img src="<?php echo url_for('frontend/assets/images/profileCamera.svg') ?>" alt="">
+                                        </button>
+                                      </div>
+                                    </div>
+                                </div>
+                                
+                                
+                                <?php }else{ ?>
+                                <div class="dont-add-profile-photo">
+                                    <div class="pv-top-card--profile-photo">
+                                        <div class="pv-top-card__profile-photo-wrapper ml0">
+                                        <div class="profile-pic-user">
+                                            <img src="<?php echo url_for($profileData->profilePic); ?>" alt="<?php echo $profileData->firstName." ".$profileData->lastName; ?>" title="<?php echo $profileData->firstName." ".$profileData->lastName; ?>">
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php } ?>
+
+                        </div>
+                        <?php if($profileId ==$user_id){ ?>
+                            <div class="right-icon">
+                                    <button class="edit-profile-icon" type="button">
+                                        <img src="<?php echo url_for('frontend/assets/images/penIcon.svg') ?>" alt="">
+                                    </button>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <div class="ph5 pb5">
+                            <div class="display-flex mt2 p-10">
+                               <div class="flex-1 mr5">
+                                        <ul class="pv-top-card--list inline-flex align-items-center">
+                                            <li class="inline t-24 t-black t-normal break-words"><?php echo $profileData->firstName." ".$profileData->lastName; ?></li>
+                                        </ul>
+                                        <h2 class="mt1 t-18 t-black t-normal break-words">
+                                             Student at University of Ghana
+                                        </h2>
+                                      </div>
+                               <div class="p-header-sch">
+                                   <h2>University Of Ghana</h2>
+                               </div>
+                            </div>
                     </div>
                 </div>
                 <aside class="profile-right-wrap"></aside>
